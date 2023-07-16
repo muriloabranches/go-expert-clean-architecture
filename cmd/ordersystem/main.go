738dc69c723813854d879e08f9c8c127ba52bd18
MuriloAbranches <muriloads@hotmail.com>
2023-07-15 23:40:28 -0300

    Added GraphQL query Orders

--- cmd/ordersystem/main.go
+++ cmd/ordersystem/main.go
@@ -65,7 +65,8 @@ func main() {
 	go grpcServer.Serve(lis)
 
 	srv := graphql_handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{
-		CreateOrderUseCase: *createOrderUseCase,
+		CreateOrderUseCase:  *createOrderUseCase,
+		FindAllOrderUseCase: *findAllOrderUseCase,
 	}}))
 	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
 	http.Handle("/query", srv)
